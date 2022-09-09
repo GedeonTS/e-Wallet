65c9ab4b935d6c06831ca3f5f8670205d1b9e37a
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-09 16:01:23 +0200

    Changed devise folder to users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'gedeonadriel@gmail.com'
+  config.mailer_sender = 'gedeontshobohwa1@gmail.com'
   config.reconfirmable = false
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
